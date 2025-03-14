b0875d70bf07ac7eda7d113300284df1b60c7fc4
maximofernandezriera <maxfernandez@gmail.com>
2025-03-14 19:22:26 +0100

    correctiones

--- GastosApi/app/Http/Controllers/AuthController.php
+++ GastosApi/app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users',
-            'password' => 'required|string|min:6'
+            'password' => 'required|string|min:6' 
         ]);
 
         $user = User::create([
